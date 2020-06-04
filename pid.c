4cabae33391357b293ec97b454a26e13c84b62fb
coronafs <coronafs@covid19>
2020-06-05 00:16:42 +0200

    dodan pid. trenutno manjka dt

---
+++ pid.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+float pid(float current, float desired)
+{
+	float p = 100;
+	float i = 0;
+	float d = 1;
+
+	static float previous=0;
+
+	float prop;
+	static float integ=0;
+	float diff;
+
+
+	prop = desired - current;
+	integ += prop;
+	diff = prop - previous;
+
+	previous = prop;
+
+	return p * prop + i * integ + d * diff;
+}
+
+
+int main()
+{
+
+	for (int i = 0; i<120; ++i) {
+		printf("%d %f \n", i, pid(i, 100));
+	}
+
+}
+
